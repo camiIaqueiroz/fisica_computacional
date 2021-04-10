662a90a3055c1995bb492447a117b3eb8b10c78b
mimieux <78580890+idontlikenumberfive@users.noreply.github.com>
2021-04-09 21:01:34 -0300

    Create Mod03Ex03.f95

---
+++ Mod03Ex03.f95
@@ -0,0 +1,21 @@
+program operacao_e
+   implicit none
+   logical :: a, b, c
+   a = .false.                     
+   b = .false.                     
+   c = a .and. b                   
+   print*, ".not.",a, ".and. (", b, ".or. (", a,".and. .not.",b,")) =", .not.a.and.(b.or.(a.and..not.b))
+   a = .false.                     
+   b = .true.                      
+   c = a .and. b                   
+   print*,".not.",a, ".and. (", b, ".or. (", a,".and. .not.",b,")) =", .not.a.and.(b.or.(a.and..not.b))
+   a = .true.                      
+   b = .false.                     
+   c = a .and. b
+   print*,".not.",a, ".and. (", b, ".or. (", a,".and. .not.",b,")) =", .not.a.and.(b.or.(a.and..not.b))
+   a = .true.                      
+   b = .true.                      
+   c = a .and. b
+   print*, ".not.",a, ".and. (", b, ".or. (", a,".and. .not.",b,")) =", .not.a.and.(b.or.(a.and..not.b))
+
+end program
